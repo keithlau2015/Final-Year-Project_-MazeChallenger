277650d3bf9dae5e92c3a7b1af20adccc85cd7c1
ShuYin Wang <amyvivi870724@gmail.com>
2019-09-13 00:10:26 +0800

    PlayerData
    hp, stamina speed hunger

---
+++ Assets/Script/playerData.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerData
+{
+    private int hp;
+
+    private int stamina;
+    private float hunger;
+    private float speed;
+
+   
+        public playerData(int hp, float hunger, float speed, int stamina) {
+            this.hp = hp;
+            this.stamina = stamina;
+            this.hunger = hunger;
+            this.speed = speed;
+    }
+        public void setHp(int hp)
+        {
+            this.hp = hp;
+
+        }
+
+        public void setStamina(int stamina)
+        {
+            this.stamina = stamina;
+
+        }
+        public void setHunger(int hunger)
+        {
+            this.hunger = hunger;
+
+        }
+        public void setSpeed(int speed)
+        {
+            this.speed = speed;
+
+        }
+
+
+        public void getHp(int hp)
+        {
+            return hp;
+
+        }
+
+        public void gsetStamina(int stamina)
+        {
+            return stamina;
+
+        }
+        public void getHunger(int hunger)
+        {
+            return hunger;
+
+        }
+        public void getSpeed(int speed)
+        {
+            return speed;
+
+        }
+
+    
+}
