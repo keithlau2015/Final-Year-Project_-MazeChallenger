3f2863c4e3b64f6e20bcf4113e2bb908efad885a
keith <s101315@gmail.com>
2019-09-13 00:04:50 +0800

    Added script
    Test Scene,
    PlayerController,
    WeaponData

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed, jumpPower;
+    private Rigidbody rigidbody;
+    private bool isGrounded;
+    private int countJump;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movenment = new Vector3(moveHorizontal, 0 , moveVertical);
+        rigidbody.AddForce(movenment * speed);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 20;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            //Double jump
+            /*
+            countJump++;
+            if (countJump<2)
+            {
+                rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+            */
+            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            countJump = 0;
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
