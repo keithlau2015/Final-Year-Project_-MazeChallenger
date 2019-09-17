9b2809a9d8d4986028d17ddb728c20c900273a1c
ShuYin Wang <amyvivi870724@gmail.com>
2019-09-17 23:30:14 +0800

    update PlayerController

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,15 +9,31 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rigidbody;
     private bool isGrounded;
     private int countJump;
+    private Camera cam;
+
+    // Raycast collider
+    private Collider firstItem = null;
+    private Dictionary<string, int> itemPriority = new Dictionary<string, int>()
+    {
+        {"Item", 0},{"Intecractable Item", 1}
+    };
+
+    private void Awake()
+    {
+        isGrounded = false;
+        countJump = 0;
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
+        cam = Camera.main;
         rigidbody = GetComponent<Rigidbody>();
     }
+   
 
-    // Update is called once per frame
-    private void Update()
+        // Update is called once per frame
+        private void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -59,6 +75,97 @@ public class PlayerController : MonoBehaviour
         if(collision.collider.tag == "Ground")
         {
             isGrounded = false;
+        }
+    }
+    private void DrawRayCastLine() {
+        //const
+        const int accuracy = 5;
+        const float radius = 0.05f;
+        const float length = 3f;
+        
+        //getting camera transformation
+        Vector3 unitForward = cam.transform.TransformDirection(Vector3.forward);
+        
+        //setting offset
+        Vector3 offset = unitForward * 1f;
+        Vector3 upCenter = cam.transform.position + offset;
+        Vector3 downCenter = downCenter + unitForward * length;
+
+        float gap = 0;
+
+        List<RaycastHit> touchItems = new List<RaycastHit>();
+        for (int level = 1; level <= accuracy; ++level)
+        {
+            float angle = 360 / (level * level);
+            for (float degree = 0; degree < 360; degree += angle)
+            {
+                float radians = degree * Math.PI / 180f;
+                Vector3 xyPoint = new Vector3(
+                    Math.cos(radius) * gap,
+                    Math.sin(radius) * gap,
+                    0
+                    );
+          
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(
+                upCenter + xyPoint,
+                unitForward,
+                out hit,
+                length
+            );
+            //debug
+            Debug.DrawLine(
+            upCenter + xyPoint,
+            downCenter + xyPoint,
+            Color.green
+            );
+            if (isHit)
+            {
+                touchItems.Add(hit);
+            }
+            };
+            gap += radius / accuracy;
+        }
+        if (touchItems.Count > 0)
+        {
+            touchItems.Sort(ObjectDistanceDetection);
+            Collider firstHit = touchItems.ElementAt(0).collider;
+            firstItem = itemPriority.ContainsKey(firstHit.tag) ? firstHit : null;
+            onTriggerLineStay();
+        }
+        else
+        {
+            firstItem = null;
+        }
+    }
+    private int ObjectDistanceDetection(RaycastHit xHit, RaycastHit yHit)
+    {
+        Collider x = xHit.collider, y = yHit.collider;
+        var xd = Vector3.Distance(cam.transform.position, xHit.point);
+        var yd = Vector3.Distance(cam.transform.position, yHit.point);
+        var cp = xd - yd;
+
+        int xPriority, yPriority;
+        xPriority = itemPriority.TryGetValue(x.tag, out xPriority) ? xPriority : int.MaxValue;
+        yPriority = itemPriority.TryGetValue(y.tag, out yPriority) ? yPriority : int.MaxValue;
+        return cp < 0 ? -1 : cp > 0 ? 1 : xPriority < yPriority ? -1 : xPriority > yPriority ? 1 : 0;
+    }
+
+    //just like onTriggerStay function
+    private void onTriggerLineStay()
+    {
+        if (!firstItem) return;
+        else if(firstItem.tag == "Weapon")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // PICK A WEAPON
+                Debug.Log("Pick a Weapon");
+            }
+        }
+        else if(firstItem.tag == "food")
+        {
+            FoodLostSystem.F_EatFood(5f);
         }
     }
 }
