508e95175ea7f099789405c763ce684e9dbde149
keith <s101315@gmail.com>
2019-09-17 21:13:10 +0800

    update playerData

--- Assets/Script/playerData.cs
+++ Assets/Script/playerData.cs
@@ -2,65 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerData
+public class PlayerData
 {
     private int hp;
 
     private int stamina;
     private float hunger;
     private float speed;
-
-   
-        public playerData(int hp, float hunger, float speed, int stamina) {
-            this.hp = hp;
-            this.stamina = stamina;
-            this.hunger = hunger;
-            this.speed = speed;
+       
+    public PlayerData(int hp, float hunger, float speed, int stamina) {
+        this.hp = hp;
+        this.stamina = stamina;
+        this.hunger = hunger;
+        this.speed = speed;
     }
-        public void setHp(int hp)
-        {
-            this.hp = hp;
 
-        }
+    public void setHp(int hp)
+    {
+        this.hp = hp;
+    }
 
-        public void setStamina(int stamina)
-        {
-            this.stamina = stamina;
+    public void setStamina(int stamina)
+    {
+        this.stamina = stamina;
+    }
 
-        }
-        public void setHunger(int hunger)
-        {
-            this.hunger = hunger;
+    public void setHunger(int hunger)
+    {
+        this.hunger = hunger;
+    }
 
-        }
-        public void setSpeed(int speed)
-        {
-            this.speed = speed;
+    public void setSpeed(int speed)
+    {
+        this.speed = speed;
+    }
 
-        }
+    public int getHp(int hp)
+    {
+        return hp;
+    }
 
+    public int getStamina(int stamina)
+    {
+        return stamina;
+    }
 
-        public void getHp(int hp)
-        {
-            return hp;
+    public int getHunger(int hunger)
+    {
+        return hunger;
+    }
 
-        }
-
-        public void gsetStamina(int stamina)
-        {
-            return stamina;
-
-        }
-        public void getHunger(int hunger)
-        {
-            return hunger;
-
-        }
-        public void getSpeed(int speed)
-        {
-            return speed;
-
-        }
-
-    
+    public int getSpeed(int speed)
+    {
+        return speed;
+    }
 }
