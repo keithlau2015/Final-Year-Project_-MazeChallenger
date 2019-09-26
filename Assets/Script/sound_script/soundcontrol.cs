6184e647560619128a58252d0f33f009e5b07eba
tomwu <tomwu@pc108.wireless29.tku.edu.tw>
2019-09-26 21:38:24 +0800

    update the sound effect script
    adding the new way to play the sound effect

---
+++ Assets/Script/sound_script/soundcontrol.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class soundcontrol : MonoBehaviour
+{
+    public Sound[] sounds;
+    public soundcontrol instance;
+    //public static AudioManager instance;
+
+	void Awake()
+	{
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+		foreach(Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+		}
+        if(sounds == null)
+        {
+            Debug.Log("Error");
+        }
+	}
+
+     public void Play(string name)
+	{
+		Sound s = Array.Find(sounds, sound => sound.name == name);
+		if(s == null)
+		{
+			Debug.Log("warnning, there is a error, name not found");
+			return;
+		}
+		s.source.Play();
+	}
+
+    //FindObjectOfType<AudioManager>().Play("spear_atk_sound");
+    // For other script using
+}
