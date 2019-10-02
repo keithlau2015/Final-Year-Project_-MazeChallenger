5e8a0cab4cabc9a06c52194e0011a7ed6d4442cd
keith <s101315@gmail.com>
2019-10-02 17:36:32 +0800

    Add few Prefabs

---
+++ Assets/Script/MonsterStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterStat : MonoBehaviour
+{
+    public int HitPoints { get; set; }
+    public float AttackSpeed { get; set; }
+    public float Speed { get; set; }
+    public int Damage { get; set; }
+    public float AttackRange { get; set; }
+    public float SpawnningRate { get; set; }
+}
