9f174e2989eae0736bef87c42b2d5ca71adb19dd
tomwu <tomwu@pc47.wireless29.tku.edu.tw>
2019-10-29 21:56:32 +0800

    add a trash scrip
    add the sound effect and add game bg and summerise the file in sound file

---
+++ Assets/Script/game_sound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<soundcontrol>().music_playing("game_bg");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
