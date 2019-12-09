781b63398ee5d4c7c3e5d6f5189b3076daa04c73
PaulTheFat <P2A3U6L7@gmail.com>
2019-12-10 00:02:28 +0800

    updated lab

---
+++ Assets/Script/VR/Drawer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Drawer"))
+        {
+            transform.SetParent(collision.transform);
+        }
+        if (collision.transform.childCount > 0)
+        {
+            if (collision.transform.GetChild(0).CompareTag("Drawer"))
+            {
+                transform.SetParent(collision.transform);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Drawer"))
+        {
+            transform.SetParent(null);
+        }
+        if (collision.transform.childCount > 0)
+        {
+            if (collision.transform.GetChild(0).CompareTag("Drawer"))
+            {
+                transform.SetParent(null);
+            }
+        }
+    }
+}
---
+++ Assets/VRGameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGameManager : MonoBehaviour
+{
+    private Vector3 gravity = new Vector3(0f, -9.89f, 0f);
+    private void Awake()
+    {
+        Physics.gravity = gravity;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
