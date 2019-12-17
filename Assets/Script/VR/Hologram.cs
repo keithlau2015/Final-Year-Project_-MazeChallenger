8be795624e9b7c369bf9835df06687d7ab08f28b
PaulTheFat <P2A3U6L7@gmail.com>
2019-12-18 01:26:15 +0800

    updated lab scene

---
+++ Assets/Script/VR/Hologram.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hologram : MonoBehaviour
+{
+    public GameObject _object;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _object.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _object.SetActive(true);
+    }
+}
