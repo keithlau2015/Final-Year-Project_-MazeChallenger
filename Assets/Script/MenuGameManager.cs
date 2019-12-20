33a6be8c885aaa99ae8765a8805e79a7a7c85446
keith <s101315@gmail.com>
2019-12-20 20:46:23 +0800

    Fix Bug

--- Assets/Script/MenuGameManager.cs
+++ Assets/Script/MenuGameManager.cs
@@ -13,8 +13,7 @@ public class MenuGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        PlayerStatus.Instance.setPlayerAtTheMenu(false);
         intoAreaIntro = "";
         HealthUI.SetActive(false);
         HungerUI.SetActive(false);
